56f8032de900c4dd47d226a0044873dbbb028184
alca2os <j.diaz26@ufromail.cl>
2021-05-10 23:55:54 -0400

    actualizacion

--- laboratorio-5/ultima_nota.cpp
+++ laboratorio-5/ultima_nota.cpp
@@ -1,23 +1,32 @@
 #include <stdio.h>
 
-float tarea1 = 0;
-float tarea2 = 0;
-float tarea3 = 0;
-float actitudinal = 0;
-float prueba1 = 0;
-float prueba2 = 0;
+float tarea1;
+float tarea2;
+float tarea3;
+float actitudinal;
+float prueba1;
+float notaminima = 4.0;
+float notaprueba2;
 
 int  main() {
     printf (" Bienvenid@ a la aplicación que sirve para calcular tu nota\n");
-    printf (" ingrese la nota de la tarea 1\n");
-    scanf ("%f", & tarea1);
-    printf ("ingrese la nota de la tarea 2\n");
-    scanf ("%f", & tarea2);
-    printf ("ingrese la nota de la tarea 3\n");
-    scanf ("%f", & tarea3);
-    printf ("ingrese la nota de la actitudinal\n");
-    scanf ("%f", & actitudinal);
-    printf ("ingrese la nota de la prueba 1\n");
-    scanf ("%f", & prueba1);
+    printf("Ingrese su nota obtenida en la Tarea 1");
+    scanf("%f", &tarea1);
+    printf("Ingrese su nota obtenida en la Tarea 2");
+    scanf("%f", &tarea2);
+    printf("Ingrese su nota obtenida en la Tarea 3");
+    scanf("%f", &tarea3);
+    printf("Ingrese su nota Actitudinal");
+    scanf("%f", &actitudinal);
+    printf("Ingrese su nota obtenida en la Prueba 1");
+    scanf("%f", &prueba1);
+    notaprueba2 = 4.0/0.35-(tarea1 * 0.28571)-(tarea2 * 0.28571)-(tarea3 * 0.28571)-(actitudinal * 0.28571)-(prueba1 * 0.714285); 
+    printf("La nota minima que se puede obtener en la Prueba 2 para aprobar el curso es %f\n", notaprueba2);
+
+    return 0;
+    
+}
+
+
 
  
